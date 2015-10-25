0ca3b0a068d0cbcba1c63ca46879d696655a5b49
lesion <lesion@autistici.org>
2015-10-25 10:37:48 +0100

    bootload

--- bootload.js
+++ bootload.js
@@ -1,5 +1,5 @@
 /*
- * BootLoader 0.2.3
+ * BootLoad 0.2.3
  * An HTML5 Hybrid Cordova/Phonegap App remote updater
  * http://github.com/lesion/remup
  * this library depends on cordova plugins File and FileTransfer
@@ -9,38 +9,52 @@
 /*globals cordova, FileTransfer */
 'use strict';
 
-window.bootloader = (function (config) {
+window.bootload = (function (){
 
-  var to_load = config.resources.length;
+  var self = {},
+    config,
+    release,
+    to_load,
+    log,
+    logger = window.logger = function logger(name){
+    return function(msg){
+      var message = "[\x1b[32m" + name + '\x1b[0m] ';
+      if(config.debug)
+      {
+        message +=  (typeof msg === 'string') ? msg : JSON.stringify(msg);
+        console.log(message);
+        request('POST',config.debug_url,message);
+      }
+    };
+  };
 
-  // check if i'm on device
-  // TOFIX: has to check for filetransfer dep
-  function ready(){
-    if(cordova_deps() || config.dev_mode)
-      load_resources();
-    else
-      alert("Error loading BootLoader! is cordova-plugin-file and cordova-plugin-file-transfer installer?");
-  }
+  log = logger('bootload');
 
-  if(on_device())
-    document.addEventListener('deviceready', ready, false);
-  // not in cordova, do not wait for deviceready event to load resources
-  else if (config.dev_mode)
-    load_resources();
+  self.boot = function(conf){
 
+    config = conf;
+    to_load = config.resources.length;
+    release = localStorage.getItem('release') || config.release;
 
-
-  function log(msg){
     if(config.debug)
-    {
-      msg = '[remup]' + (typeof msg === 'string') ? msg : JSON.stringify(msg);
-      console.log(msg);
-      request('POST','http://elementary.local:10000',msg);
-    }
-  }
+      window.onerror = window.logger('GLOBAL');
 
-  window.onerror = log;
-  window.log = log;
+    // check if i'm on device
+    function ready(){
+      if(cordova_deps() || config.dev_mode)
+        load_resources();
+      else
+        alert("Error loading BootLoad! is cordova-plugin-file and cordova-plugin-file-transfer installer?");
+    }
+
+    if(on_device())
+      document.addEventListener('deviceready', ready, false);
+    // not in cordova, do not wait for deviceready event to load resources
+    else if (config.dev_mode)
+      load_resources();
+  };
+
+
 
   function on_device(){
     return ( document.URL.indexOf( 'http://' ) === -1 );
@@ -60,14 +74,12 @@ window.bootloader = (function (config) {
     // load resource from config.dev_url in dev_mode
     // load resource from cordova fs the other case
     // in case of error, load the resource from bundle !!
-
-    
     var base_path;
 
     if(config.dev_mode)
       base_path = config.dev_url;
-    else
-      base_path = cordova.file.documentsDirectory;
+    else // FIXME i'm removing it now (19 oct, first release) because loading from fs is not ready!
+      base_path = '';//cordova.file.documentsDirectory;
 
     load(base_path + resource)
       .then(success)
@@ -99,8 +111,15 @@ window.bootloader = (function (config) {
 
     function success(){
       log("Succesfully loaded " + base_path + resource );
-      if(!--to_load)
-        config.oncomplete();
+      if(--to_load) return;
+
+      config.oncomplete();
+      //self.check_update().then(function(data){
+        //log(data.resources);
+        //data.resources.forEach(function(resource){
+          //download(resource).then(log).catch(log);
+        //});
+      //}).catch(log);
     }
 
   }
@@ -115,45 +134,48 @@ window.bootloader = (function (config) {
     });
   }
 
+
+
   /* async check for updates */
-  function check_update(current_release_name, manifest_uri, success_callback, error_callback) {
-    //ajax call to check if library as to be updated
-    var xhr = new XMLHttpRequest();
-    console.log("Dentro check_release");
-    xhr.open("GET", manifest_uri + '?' + Math.random(), true); // true mean async
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState !== 4) return;
-      if (xhr.status !== 200) {
-        error_callback(xhr.status + ' ' + xhr.statusText);
-        return;
-      }
-      var new_release_meta = {};
-      new_release_meta = JSON.parse(xhr.responseText);
-      new_release_meta.is_new = (new_release_meta.release !== current_release_name);
-      success_callback(new_release_meta);
-    };
-    xhr.send(null); // null is the body of the GET
-  }
+  self.check_update = function check_update() {
+    return new Promise(function(resolve,reject){
 
-  function update(release_meta, success_update, error_update) {
-    // call me when there is a new release
-    var ft = new FileTransfer();
-    ft.download(release_meta.uri, "cdvfile://localhost/persistent/" + config.main,
-        success_update, error_update);
-  }
+      var update_url = config.dev_url + 'bootload.json';
 
-  function restart() {
-    location.reload();
-  }
-
-  return {
-    update: update,
-    restart: restart,
-    check_update: check_update,
+      //ajax call to check if library as to be updated
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET", update_url + '?' + Math.random(), true); // true mean async
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState !== 4) return;
+        if (xhr.status !== 200) {
+          reject(xhr.status + ' ' + xhr.statusText);
+          return;
+        }
+        var new_release_meta = {};
+        new_release_meta = JSON.parse(xhr.responseText);
+        log(new_release_meta.release  + ' ' + release);
+        if(new_release_meta.release !== release) 
+          resolve(new_release_meta);
+        else
+          reject('No update, same release');
+      };
+      xhr.send(null); // null is the body of the GET
+    });
   };
 
-})();
+  function download(resource) {
+    return new Promise(function(resolve,reject){
+      var ft = new FileTransfer();
+      log("Downloading " + config.dev_url + resource );
+      ft.download(config.dev_url + resource, "cdvfile://localhost/persistent/" + resource,
+          resolve, reject);
+    });
+  }
 
-//remup.init();
-//document.addEventListener("deviceready", remup.init, false);
-//
+  self.restart = function restart() {
+    location.reload();
+  };
+
+  return self;
+
+})();
